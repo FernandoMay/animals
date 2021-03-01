1554f5549cbac2172db26c0c4af0a5e6b3faa502
Fernando May <fmay130@gmail.com>
2021-03-01 00:01:55 -0600

    alexa

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:animals/constants.dart';
+import 'package:animals/responsive.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -7,94 +8,163 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-        width: size.width,
-        height: size.height,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Header(),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Padding(
-                      padding: EdgeInsets.only(right: 40),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          RichText(
-                            text: TextSpan(
-                              children: [
-                                TextSpan(
-                                  text: 'Buy ',
-                                  style: TextStyle(
-                                    fontSize: 32,
-                                    fontWeight: FontWeight.w800,
-                                  ),
+      endDrawer: ConstrainedBox(
+        child: SideMenu(),
+        constraints: BoxConstraints(maxWidth: 300),
+      ),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Container(
+            width: size.width,
+            constraints: BoxConstraints(
+              minHeight: size.height,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Header(),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                              right: !isMobile(context) ? 40 : 0),
+                          child: Column(
+                            crossAxisAlignment: !isMobile(context)
+                                ? CrossAxisAlignment.start
+                                : CrossAxisAlignment.center,
+                            mainAxisAlignment: !isMobile(context)
+                                ? MainAxisAlignment.start
+                                : MainAxisAlignment.center,
+                            children: [
+                              if (isMobile(context))
+                                Image.asset(
+                                  'lib/assets/animal.png',
+                                  height: size.height * 0.3,
                                 ),
-                                TextSpan(
-                                  text: 'Cruelty Free',
-                                  style: TextStyle(
-                                    fontSize: 32,
-                                    fontWeight: FontWeight.w800,
+                              RichText(
+                                text: TextSpan(
+                                  children: [
+                                    TextSpan(
+                                      text: 'Buy ',
+                                      style: TextStyle(
+                                        fontSize: 32,
+                                        fontWeight: FontWeight.w800,
+                                      ),
+                                    ),
+                                    TextSpan(
+                                      text: 'Cruelty Free',
+                                      style: TextStyle(
+                                        fontSize: 32,
+                                        fontWeight: FontWeight.w800,
+                                        color: primaryColor,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Text(
+                                'Makeup Products',
+                                style: TextStyle(
+                                  fontSize: 32,
+                                  fontWeight: FontWeight.w800,
+                                ),
+                              ),
+                              Text(
+                                'Online Today',
+                                style: TextStyle(
+                                  fontSize: 32,
+                                  fontWeight: FontWeight.w800,
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Text(
+                                'Help us in making this world a better place for animals.',
+                                textAlign: isMobile(context)
+                                    ? TextAlign.center
+                                    : TextAlign.start,
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w300,
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Wrap(
+                                runSpacing: 10,
+                                children: [
+                                  MainButton(
+                                    title: 'GET STARTED',
+                                    tapEvent: () {},
                                     color: primaryColor,
                                   ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          Text(
-                            'Makeup Products',
-                            style: TextStyle(
-                              fontSize: 32,
-                              fontWeight: FontWeight.w800,
-                            ),
-                          ),
-                          Text(
-                            'Online Today',
-                            style: TextStyle(
-                              fontSize: 32,
-                              fontWeight: FontWeight.w800,
-                            ),
-                          ),
-                          SizedBox(height: 10),
-                          Text(
-                            'Help us in making this world a better place for animals.',
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.w300,
-                            ),
-                          ),
-                          SizedBox(height: 10),
-                          Row(
-                            children: [
-                              MainButton(
-                                title: 'GET STARTED',
-                                tapEvent: () {},
-                                color: primaryColor,
-                              ),
-                              SizedBox(width: 10),
-                              MainButton(
-                                title: 'WATCH VIDEO',
-                                tapEvent: () {},
-                                color: secondaryColor,
+                                  SizedBox(width: 10),
+                                  MainButton(
+                                    title: 'WATCH VIDEO',
+                                    tapEvent: () {},
+                                    color: secondaryColor,
+                                  ),
+                                ],
                               ),
                             ],
                           ),
-                        ],
+                        ),
                       ),
-                    ),
+                      if (isDesktop(context) || isTab(context))
+                        Expanded(
+                          child: Image.asset(
+                            'lib/assets/animal.png',
+                            height: size.height * 0.7,
+                          ),
+                        ),
+                    ],
                   ),
-                  Expanded(child: Image.asset('lib/assets/animal.png'))
-                ],
-              ),
+                ),
+                (!isMobile(context)) ? Footer() : MobileFooter(),
+              ],
             ),
-            Footer(),
-          ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SideMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      color: Colors.white,
+      child: SafeArea(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              NavItem(
+                title: 'Home',
+                tapEvent: () {},
+              ),
+              NavItem(
+                title: 'Donate',
+                tapEvent: () {},
+              ),
+              NavItem(
+                title: 'Contact Us',
+                tapEvent: () {},
+              ),
+              NavItem(
+                title: 'Login',
+                tapEvent: () {},
+              ),
+              NavItem(
+                title: 'Shop',
+                tapEvent: () {},
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -137,6 +207,44 @@ class Footer extends StatelessWidget {
                 ),
               ],
             ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MobileFooter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+      child: Column(
+        children: [
+          Text(
+            'All Rights Reserved',
+            style: TextStyle(fontSize: 10),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SocialItem(
+                title: 'Twitter',
+                tapEvent: () {},
+              ),
+              SocialItem(
+                title: 'Facebook',
+                tapEvent: () {},
+              ),
+              SocialItem(
+                title: 'Linkedin',
+                tapEvent: () {},
+              ),
+              SocialItem(
+                title: 'Instagram',
+                tapEvent: () {},
+              ),
+            ],
           ),
         ],
       ),
@@ -259,26 +367,38 @@ class Header extends StatelessWidget {
             ),
           ),
           Spacer(),
-          NavItem(
-            title: 'Home',
-            tapEvent: () {},
-          ),
-          NavItem(
-            title: 'Donate',
-            tapEvent: () {},
-          ),
-          NavItem(
-            title: 'Contact Us',
-            tapEvent: () {},
-          ),
-          NavItem(
-            title: 'Login',
-            tapEvent: () {},
-          ),
-          NavItem(
-            title: 'Shop',
-            tapEvent: () {},
-          ),
+          if (!isMobile(context))
+            Row(
+              children: [
+                NavItem(
+                  title: 'Home',
+                  tapEvent: () {},
+                ),
+                NavItem(
+                  title: 'Donate',
+                  tapEvent: () {},
+                ),
+                NavItem(
+                  title: 'Contact Us',
+                  tapEvent: () {},
+                ),
+                NavItem(
+                  title: 'Login',
+                  tapEvent: () {},
+                ),
+                NavItem(
+                  title: 'Shop',
+                  tapEvent: () {},
+                ),
+              ],
+            ),
+          if (isMobile(context))
+            IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {
+                Scaffold.of(context).openEndDrawer();
+              },
+            ),
         ],
       ),
     );
